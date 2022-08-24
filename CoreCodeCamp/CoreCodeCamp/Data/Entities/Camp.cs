57c995bbda51da12d0c13e52de8bbc2f3ec9b2a9
anais di genua <anais.digenua5@gmail.com>
2022-08-24 15:37:15 +0200

    add entities

---
+++ CoreCodeCamp/CoreCodeCamp/Data/Entities/Camp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreCodeCamp.Data.Entities
+{
+    public class Camp
+    {
+        public int CampId { get; set; }
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
+
+        //Navigation Properties
+        public Location Location { get; set; }
+        public ICollection<Talk> Talks { get; set; }
+    }
+}
