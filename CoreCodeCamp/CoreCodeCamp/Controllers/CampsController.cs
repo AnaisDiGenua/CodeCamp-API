272265cd2532f426f46285ad2be9c133f6171793
anais di genua <anais.digenua5@gmail.com>
2022-08-25 16:09:20 +0200

    add camp controller

---
+++ CoreCodeCamp/CoreCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            if (false) return BadRequest("bad request :(");
+
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code" });
+        }
+    }
+}
