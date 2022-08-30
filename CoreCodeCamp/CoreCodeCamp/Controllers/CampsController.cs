d16f3da74932ac44bb37c4d78c8bc680e96ef65c
anais di genua <anais.digenua5@gmail.com>
2022-08-30 11:57:29 +0200

    add methods to CampController

--- CoreCodeCamp/CoreCodeCamp/Controllers/CampsController.cs
+++ CoreCodeCamp/CoreCodeCamp/Controllers/CampsController.cs
@@ -1,16 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
     public class CampsController : Controller
     {
-        [HttpGet]
-        public IActionResult GetCamps()
-        {
-            if (false) return BadRequest("bad request :(");
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
 
-            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code" });
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync(includeTalks);
+
+                //ritorno un CampModel a partire dal risultato che è una entità Camp
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+
+                if (result == null) return NotFound();
+
+                return _mapper.Map<CampModel>(result);
+
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsByEventDate(theDate, includeTalks);
+
+                if (!results.Any()) return NotFound();
+
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
         }
     }
 }
