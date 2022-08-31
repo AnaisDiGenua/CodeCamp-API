d1caf90c643d130a356a5d85045976b81f0714dc
anais di genua <anais.digenua5@gmail.com>
2022-08-31 12:00:25 +0200

    add operationsController

---
+++ CoreCodeCamp/CoreCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    public class OperationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
