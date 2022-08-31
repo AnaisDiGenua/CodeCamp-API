3a495a95a2ee56cd813efd294d673220d8db96a8
anais di genua <anais.digenua5@gmail.com>
2022-08-31 15:27:57 +0200

    add operationsController methods

--- CoreCodeCamp/CoreCodeCamp/Controllers/OperationsController.cs
+++ CoreCodeCamp/CoreCodeCamp/Controllers/OperationsController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace CoreCodeCamp.Controllers
 {
-    public class OperationsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
         {
-            return View();
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
