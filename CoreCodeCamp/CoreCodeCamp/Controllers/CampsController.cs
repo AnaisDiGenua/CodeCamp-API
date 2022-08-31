9da91909494b5855f58361fff170c4b2e95f7fc0
anais di genua <anais.digenua5@gmail.com>
2022-08-31 11:51:42 +0200

    add talkscontroller

--- CoreCodeCamp/CoreCodeCamp/Controllers/CampsController.cs
+++ CoreCodeCamp/CoreCodeCamp/Controllers/CampsController.cs
@@ -4,6 +4,7 @@ using CoreCodeCamp.Data.Entities;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
--- CoreCodeCamp/CoreCodeCamp/Data/CampProfile.cs
+++ CoreCodeCamp/CoreCodeCamp/Data/CampProfile.cs
@@ -10,7 +10,16 @@ namespace CoreCodeCamp.Data
         {
             this.CreateMap<Camp, CampModel>()
                 //per mappare una proprietà dall'entità location
-              .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+              .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+              .ReverseMap();
+
+            this.CreateMap<Talk, TalkModel>()
+               .ReverseMap()
+              .ForMember(t => t.Camp, opt => opt.Ignore())
+              .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>()
+              .ReverseMap();
         }
     }
 }
